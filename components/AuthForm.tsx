26e8c32efa9c71165a32d7daa439d8316afcb6be
daffatahtaa <daffatahta12@gmail.com>
2024-10-10 21:30:57 +0700

    Fix auth, sign in, sign up form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,88 +1,195 @@
-"use client"
+"use client";
 
-import Image from 'next/image'
-import Link from 'next/link'
-import React, { useState } from 'react'
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import CustomInput from './CustomInput'
-import { authFormSchema } from '@/lib/utils'
-
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import CustomInput from "./CustomInput";
+import { authFormSchema } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
 
 const AuthForm = ({ type }: { type: string }) => {
+  const [user, setuser] = useState(null);
+  const [isLoading, setisLoading] = useState(false);
 
-    const [user, setuser] = useState(null)
+  const formSchema = authFormSchema(type);
 
-    // 1.
-    const form = useForm<z.infer<typeof authFormSchema>>({
-        resolver: zodResolver(authFormSchema),
-        defaultValues: {
-            email: "",
-            password: "",
+  // 1.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-        }
-    })
+  // 2.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    setisLoading(true);
+    console.log(values);
+    setisLoading(false);
+  }
 
-    // 2.
-    function onSubmit(values: z.infer<typeof authFormSchema>) {
-        console.log(values)
-    }
+  return (
+    <section className="auth-form">
+      <header className="flex flex-col gap-5 md:gap-8">
+        <Link href="/" className="cursor-pointer flex items-center gap-1">
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Horizon Logo"
+          />
+          <h1 className="text-26 font-ibm-plex-serif font-bold -text-black-1">
+            Horizon
+          </h1>
+        </Link>
 
-    return (
-        <section className='auth-form'>
-            <header className='flex flex-col gap-5 md:gap-8'>
-                <Link href='/' className='cursor-pointer flex items-center gap-1'>
-                    <Image src='/icons/logo.svg'
-                        width={34}
-                        height={34}
-                        alt='Horizon Logo'
-                    />
-                    <h1 className='text-26 font-ibm-plex-serif font-bold -text-black-1'>Horizon</h1>
-                </Link>
-
-                <div className='flex flex-col gap-1 md:gap-3'>
-                    <h1 className='text-24 lg:text-36 font-semibold text-gray-900'>
-                        {user ? 'Link Account' : type === 'sign-in' ? "sign-in" : "sign-up"}
-                        <p className='text-16 font-normal text-gray-600'>
-                            {user ? 'Link your account to get started' : "Please enter your details"}
-                        </p>
-                    </h1>
-                </div>
-            </header>
-            {user ? (
-                <div className='flex flex-col gap-4'>
-                    {/* PlaidLink */}
-                </div>
-            ) : (
+        <div className="flex flex-col gap-1 md:gap-3">
+          <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
+            {user ? "Link Account" : type === "sign-in" ? "Sign in" : "Sign up"}
+            <p className="text-16 font-normal text-gray-600">
+              {user
+                ? "Link your account to get started"
+                : "Please enter your details"}
+            </p>
+          </h1>
+        </div>
+      </header>
+      {user ? (
+        <div className="flex flex-col gap-4">{/* PlaidLink */}</div>
+      ) : (
+        <>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              {type === "sign-up" && (
                 <>
-                    <Form {...form}>
-                        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
-                            <CustomInput
-                                control={form.control} name="email" label='Email' placeholder='Enter your email' />
+                  <CustomInput
+                    control={form.control}
+                    name="firstName"
+                    label="First Name"
+                    placeholder="Enter your first name"
+                  />
 
-                            <CustomInput
-                                control={form.control} name="password" label='Password' placeholder='Enter your password' />
+                  <CustomInput
+                    control={form.control}
+                    name="lastName"
+                    label="Last Name"
+                    placeholder="Enter your last name"
+                  />
 
-                            <Button type='submit'>Submit</Button>
-                        </form>
-                    </Form>
+                  <CustomInput
+                    control={form.control}
+                    name="address1"
+                    label="Address"
+                    placeholder="Enter your specific address"
+                  />
+
+                  <CustomInput
+                    control={form.control}
+                    name="city"
+                    label="State"
+                    placeholder="ex: NY"
+                  />
+
+                  <CustomInput
+                    control={form.control}
+                    name="postalCode"
+                    label="First Name"
+                    placeholder="11101"
+                  />
+
+                  <CustomInput
+                    control={form.control}
+                    name="dateOfBirth"
+                    label="Date of Birth"
+                    placeholder="dd-mm-yyyy"
+                  />
+
+                  <CustomInput
+                    control={form.control}
+                    name="ssn"
+                    label="SSN"
+                    placeholder="ex: 1234"
+                  />
+
+                  <CustomInput
+                    control={form.control}
+                    name="email"
+                    label="Email"
+                    placeholder="Enter your email"
+                  />
+
+                  <CustomInput
+                    control={form.control}
+                    name="password"
+                    label="Password"
+                    placeholder="Enter your password"
+                  />
                 </>
-            )}
-        </section>
-    )
-}
+              )}
 
-export default AuthForm
+              <CustomInput
+                control={form.control}
+                name="email"
+                label="Email"
+                placeholder="Enter your email"
+              />
+
+              <CustomInput
+                control={form.control}
+                name="password"
+                label="Password"
+                placeholder="Enter your password"
+              />
+
+              <div className="flex flex-col gap-4">
+                <Button className="form-btn" disabled={isLoading} type="submit">
+                  {isLoading ? (
+                    <>
+                      <Loader2 size={20} className="animate-spin" /> &nbsp;
+                      Loading...
+                    </>
+                  ) : type === "sign-in" ? (
+                    "Sign in"
+                  ) : (
+                    "Sign up"
+                  )}
+                </Button>
+              </div>
+            </form>
+          </Form>
+          <footer className="flex justify-center gap-1">
+            <p className="text-14 font-normal text-gray-600">
+              {type === "sign-in"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+            </p>
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="form-link"
+            >
+              {type === "sign-in" ? "Sign up" : "Sign in"}
+            </Link>
+          </footer>
+        </>
+      )}
+    </section>
+  );
+};
+
+export default AuthForm;
--- lib/utils.ts
+++ lib/utils.ts
@@ -195,19 +195,22 @@ export const getTransactionStatus = (date: Date) => {
   return date > twoDaysAgo ? "Processing" : "Success";
 };
 
-export const authFormSchema = z.object({
-  // Sign Up
+export const authFormSchema = (type: string) =>
+  z.object({
+    // Sign Up
 
-  // firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
-  // lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
-  // address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
-  // city: type === 'sign-in' ? z.string().optional() : z.string().max(50),
-  // state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(50),
-  // postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
-  // dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
-  // ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+    firstName: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    lastName: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    address1: type === "sign-in" ? z.string().optional() : z.string().max(50),
+    city: type === "sign-in" ? z.string().optional() : z.string().max(50),
+    state:
+      type === "sign-in" ? z.string().optional() : z.string().min(2).max(50),
+    postalCode:
+      type === "sign-in" ? z.string().optional() : z.string().min(3).max(6),
+    dateOfBirth: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    ssn: type === "sign-in" ? z.string().optional() : z.string().min(3),
 
-  // both
-  email: z.string().email(),
-  password: z.string().min(8)
-})
+    // both
+    email: z.string().email(),
+    password: z.string().min(8),
+  });
