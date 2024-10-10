33d057cb8dfade16d6580a82f7a48a649e2fa10b
daffatahtaa <daffatahta12@gmail.com>
2024-10-10 12:30:35 +0700

    Fix sidebar active link

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -24,7 +24,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
                 </Link>
                 {sidebarLinks.map((item) => {
 
-                    const isActive = pathname === item.route || pathname.toString().startsWith(`${item.route}/`)
+                    const isActive = pathname() === item.route || pathname.toString().startsWith(`${item.route}/`)
 
                     return (
                         <Link className={cn('sidebar-link', {'bg-bank-gradient': isActive})} href={item.route} key={item.label}>
